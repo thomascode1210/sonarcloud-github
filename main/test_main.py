428483cd4413fca45ff202a8aaffbbf7de002e79
thomascode1210 <thomas.code1210@gmail.com>
2025-05-21 15:40:31 +0700

    add test_main.py

---
+++ main/test_main.py
@@ -0,0 +1,14 @@
+import unittest
+from main import greet_user, calculate_sum
+
+class TestMain(unittest.TestCase):
+    def test_greet_user(self):
+        self.assertEqual(greet_user("Tuấn Anh"), "Xin chào, Tuấn Anh!")
+        self.assertEqual(greet_user(""), "Xin chào, bạn!")
+
+    def test_calculate_sum(self):
+        self.assertEqual(calculate_sum(3, 4), 7)
+        self.assertEqual(calculate_sum(-1, 1), 0)
+
+if __name__ == "__main__":
+    unittest.main()
