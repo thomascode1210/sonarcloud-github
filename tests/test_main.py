10287756fc33cd523bf090ff99dc8dcfe77e5aff
thomascode1210 <thomas.code1210@gmail.com>
2025-05-23 11:33:54 +0700

    edit code test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
-# test_main.py
 import pytest
-from main import greet_user, calculate_sum
+from main import greet_user, calculate_sum, main
+from unittest.mock import patch
 
 def test_greet_user_with_name():
     assert greet_user("An") == "Xin chào, An!"
@@ -13,3 +13,13 @@ def test_calculate_sum_positive():
 
 def test_calculate_sum_negative():
     assert calculate_sum(-1, -5) == -6
+
+def test_main(monkeypatch, capsys):
+    inputs = iter(["Minh", "4", "5"])
+
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+
+    captured = capsys.readouterr()
+    assert "Xin chào, Minh!" in captured.out
+    assert "Tổng của 4 và 5 là: 9" in captured.out
